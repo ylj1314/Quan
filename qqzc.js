cb919cae5b8a5118405f6e7483f6afff23f8da3a
Ces <1239741325@qq.com>
2020-12-11 13:56:32 +0800

    hh
    Hh

--- qqzc.js
+++ qqzc.js
@@ -3,12 +3,12 @@ const senku=init()
 const url = "https://task.qq.com/index.php/api/checkinDaily?taskId=2657&type=2";
 const body = "";
 const headers = {
-    "Cookie": "access_token=EA69989A38E035B329DDBB163CC05ABC; appid=1105152396; openid=FD0688596EC940A55E54F8CE5DAC402C; useopenid=1; laravel_session=eyJpdiI6InhMb1FmTHluOExjeXJ6U01ZTlU2Nk44aDZwYnJTTjZ6NjBGOWk2c0h3eUU9IiwidmFsdWUiOiJqSzJqM3VyaEREQWk1ZXQwTk8wdktacVZZUkdLc0lDVnA1d2ZOWGJoVTRuaXhLK2x1WDVMK2dDYjVBVDhQOWI0VmN2K0VmZWsrRHRhY3pNaU1oNWg3dz09IiwibWFjIjoiZGYxOWNiNTQ3Mjc3OWVhZGJkYWIyNWI0MTZlN2M2NDY5NDQ5YWZlMTUxNzRjYjlmYzIyZTBjZDQ5NTNhYjAzMSJ9; tphp_arr_nick=917326587%7C; access_token=EA69989A38E035B329DDBB163CC05ABC; appid=1105152396; openid=FD0688596EC940A55E54F8CE5DAC402C; useopenid=1",
+    "Cookie": "access_token=64540CB1C11E116C0CE31FD38A6D3711; appid=1105152396; openid=9FA064824F647D864193E9EA75E3316C; useopenid=1; laravel_session=eyJpdiI6IlhKeUVVOTludHVvWlBFMlM5c0xYXC9GVnhEeGx5STRUXC9mYWllWjRVanJqaz0iLCJ2YWx1ZSI6IlRockRVNTB6ZlIyU1hLZ1JjSkROWGFTRHBZVU5wNVdjR3RDalFzYUN0eEVjQWN2ZkkyS1BmdVk5SUZhRnMxZVoxRXF4YWI2WXA3WE0rWjZ0ZE1xXC85QT09IiwibWFjIjoiZGU4MjVkOWZjYjU1ZDdiMTUzYjQ4MDgzZTVkY2Y5MGI0YjI0ZTAzNjAzYjY2YjUzNDNkZDQ4YzQwYjJkZmVhYyJ9; tphp_arr_nick=277001038%7C; access_token=64540CB1C11E116C0CE31FD38A6D3711; appid=1105152396; openid=9FA064824F647D864193E9EA75E3316C; useopenid=1",
     /*"User-Agent": "Mozilla/5.0 (iPad; CPU OS 14_0 like Mac OS X) AppleWebKit/605.1.15 (KHTML, like Gecko) Mobile/15E148",*/
    /* "X-Requested-With": "XMLHttpRequest",
     "Host": "task.qq.com",
     "Referer": "https://task.qq.com/index.php/appWebSignin",*/
-    "X-CSRF-TOKEN": "sliaBOXInrt7JFcK1vgISMbH3DACGDf4G5gbXm02"
+    "X-CSRF-TOKEN": "X-CSRF-TOKEN: SAQMYApoiJtgM22MoD1p90osLZrPWaTTuIQ9FI3a"
 };
 const request = {
     url: url,
